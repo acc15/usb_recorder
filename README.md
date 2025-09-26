d0d3acb96c523fd47af2ac9575394e049f70bf6f
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2025-09-26 20:42:51 +0300

    data framing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # usb_recorder
+
+Tiny utility program for reading USB serial data to file.
+
+This utility exists because neither `picocom` nor `minicom` can't flush input USB buffer to drop junk bytes before start
--- src/main.c
+++ src/main.c
@@ -45,10 +45,17 @@ void usb_recorder_free(usb_recorder* c) {
     }
 }
 
+const uint8_t DATA_SIGNATURE[] = {0x77, 0xaf, 0x01, 0xfe};
+const size_t DATA_HEADER_SIZE = sizeof(DATA_SIGNATURE) + sizeof(uint32_t);
+
 int parse_args(usb_recorder* c, int argc, char **argv) {
 
-    c->bufsize = 4096;
-    c->timeout = 1000;
+    c->bufsize = (DATA_HEADER_SIZE + 1024) * 4;
+    c->timeout = 10000;
+    c->databits = 8;
+    c->parity = SP_PARITY_NONE;
+    c->stopbits = 1;
+    c->baudrate = 9600;
     c->port_name = NULL;
     c->path = NULL;
 
@@ -199,7 +206,6 @@ int init_port( usb_recorder* c ) {
     int code;
     if ((code = sp_err( sp_get_port_by_name(c->port_name, &c->port) )) != 0) { return code; }
 
-
     struct sp_port* port = c->port;
     if ((code = sp_err( sp_open(port, SP_MODE_READ) )) != 0) { return code; }
     c->opened = true;
@@ -215,7 +221,7 @@ int init_port( usb_recorder* c ) {
 int main(int argc, char **argv) {
     usb_recorder c = {0};
     
-    int exit_code;
+    int exit_code = 0;
     if ((exit_code = parse_args(&c, argc, argv)) != 0) {
         return exit_code;
     }
@@ -224,8 +230,8 @@ int main(int argc, char **argv) {
         return exit_code;
     }
 
-    uint8_t* data = malloc(c.bufsize);
-    if (data == NULL) {
+    c.buf = malloc(c.bufsize);
+    if (c.buf == NULL) {
         fprintf(stderr, "Can't allocate buffer for reading data");
         usb_recorder_free(&c);
         return -1;
@@ -239,21 +245,72 @@ int main(int argc, char **argv) {
     }
 
     exit_code = sp_err( sp_flush(c.port, SP_BUF_INPUT) );
-    while (exit_code == 0) {
-        enum sp_return ret = sp_blocking_read(c.port, data, c.bufsize, c.timeout);
+    
+    size_t block_offset = 0;
+    size_t block_size = 0; // sizeof block to read
+    while (true) {
+        
+        enum sp_return ret = sp_blocking_read(c.port, c.buf, 
+            block_offset >= DATA_HEADER_SIZE && (block_size - block_offset) < c.bufsize 
+                ? block_size - block_offset 
+                : c.bufsize, 
+            c.timeout);
+
         if (ret < 0) {
             exit_code = sp_err(ret);
             break;
         }
-        size_t r_bytes = (size_t) ret;
-        size_t w_bytes = fwrite(data, 1, r_bytes, c.file);
-        printf("%zu bytes was read, %zu bytes written\n", r_bytes, w_bytes);
-        if (w_bytes != r_bytes) {
-            fprintf(stderr, "Can't write data to file: %s; Error: %s", c.path, strerror(errno));
-            break;
+
+        const size_t read_size = (size_t) ret;
+        size_t read_offset = 0;
+        while (read_offset < read_size) {
+            
+            if (block_offset < sizeof(DATA_SIGNATURE)) {
+                
+                if (DATA_SIGNATURE[block_offset] != c.buf[read_offset]) {
+                    block_offset = 0;
+                } else {
+                    ++block_offset;
+                }
+                
+                ++read_offset;
+
+            } else if (block_offset < DATA_HEADER_SIZE) {
+                
+                if (block_offset == sizeof(DATA_SIGNATURE)) {
+                    block_size = c.buf[read_offset];
+                } else {
+                    block_size |= (c.buf[read_offset] << (8 * (block_offset - sizeof(DATA_SIGNATURE))));
+                }
+
+                ++read_offset;
+                ++block_offset;
+
+            } else {
+
+                const size_t block_remaining = block_size - block_offset;
+                const size_t read_remaining = read_size - read_offset;
+
+                const size_t write_size = block_remaining > read_remaining ? read_remaining : block_remaining;
+                if (fwrite(c.buf + read_offset, 1, write_size, c.file) != write_size) {
+                    fprintf(stderr, "Can't write data to file: %s; Error: %s", c.path, strerror(errno));
+                    break;
+                }
+                printf("%zu bytes written\n", write_size);
+
+                read_offset += write_size;
+                block_offset += write_size;
+
+                if (block_offset >= block_size) {
+                    block_offset = 0;
+                }
+
+            }
+
         }
+
     }
 
     usb_recorder_free(&c);
-    return 0;
+    return exit_code;
 }
